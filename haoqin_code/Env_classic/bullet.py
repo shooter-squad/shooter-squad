8142c5eb2b9e7ff22f036030cb8b54b75a21d26c
Haoqin Deng <haoqinde@usc.edu>
2021-11-07 15:19:38 -0800

    classic env

---
+++ haoqin_code/Env_classic/bullet.py
@@ -0,0 +1,31 @@
+from typing import Tuple
+
+import pygame
+
+from Env.constants import *
+
+
+class Bullet(pygame.sprite.Sprite):
+    """
+    The bullet class.
+    """
+
+    def __init__(self, centerx: int, centery: int, color: Tuple[int, int, int], vel_x: int, vel_y: int,
+                 screen_rect: pygame.Rect):
+        super().__init__()
+        self.image = pygame.Surface((BULLET_WIDTH, BULLET_HEIGHT)).convert()
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = centerx
+        self.rect.centery = centery
+        self.vel_x = vel_x
+        self.vel_y = vel_y
+        self.screen_rect = screen_rect
+
+    def update(self):
+        self.rect.x += self.vel_x
+        self.rect.y += self.vel_y
+
+        # Kill if it goes off screen
+        if not self.screen_rect.colliderect(self.rect):
+            self.kill()
